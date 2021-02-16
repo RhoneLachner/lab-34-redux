885af4b6a4cbc5d856b3a127aa82e929afd1ff09
Rhone Lachner <fernandclay@gmail.com>
2021-02-16 14:29:45 -0800

    first test passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
 
-describe('App component', () => {
+
+
+describe('useRecord function', () => {
+  it ('changes the background color', async() => {
+    render(<App />);
+    const colorInput = screen.getByLabelText('color input');
+    fireEvent.change(colorInput, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+    const div = await screen.getByTestId('colorDiv');
+    expect(div).toHaveStyle({
+      backgroundColor: '#00FF00'
+    });
+ 
+  
+  });
+});
+
+
+
+
+describe('App component snapshot', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
     const { asFragment } = render(<App />);
