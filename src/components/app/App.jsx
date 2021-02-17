eaba732655a94454761ba0b1bf39b11d42691d30
Rhone Lachner <fernandclay@gmail.com>
2021-02-16 18:34:56 -0800

    removed whitespace and unused comments. all tests passing!!

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -35,34 +35,3 @@ const App = () => {
 };
 
 export default App;
-
-
-// const useRecord = (init) => {
-//   const [before, setBefore] = useState([]);
-//   const [current, setCurrent] = useState(init);
-//   const [after, setAfter] = useState([]);
-
-//   const undo = () => {
-//     setAfter(after => [current, ...after]);
-//     setCurrent(before[before.length - 1]);
-//     setBefore(before => before.slice(0, -1));
-//   };
-
-//   const redo = () => {
-//     setBefore(before => [...before, current]);
-//     setCurrent(after[0]);
-//     setAfter(after => after.slice(1));
-//   };
-
-//   const record = val => {
-//     setBefore(before => [...before, current]);
-//     setCurrent(val);
-//   };
-
-//   return {
-//     undo,
-//     record,
-//     redo,
-//     current,
-//   };
-// };
